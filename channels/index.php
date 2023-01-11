f2a87326dc86cb472a9443355e549b507fcd0e40
Bram Matthys <syzop@vulnscan.org>
2023-01-11 10:06:13 +0100

    Channel list: reorder fields a bit, drop topic set by/on

--- channels/index.php
+++ channels/index.php
@@ -17,11 +17,10 @@ $channels = $rpc->channel()->getAll();
 <table class="table table-responsive caption-top table-striped">
 	<thead class="table-primary">
 	<th>Name</th>
-	<th>Created</th>
-	<th>User count</th>
-	<th>Topic</th>
-	<th>Topic Set</th>
+	<th>Users</th>
 	<th>Modes</th>
+	<th>Topic</th>
+	<th>Created</th>
 </thead>
 <tbody>
 	<?php
@@ -29,14 +28,12 @@ $channels = $rpc->channel()->getAll();
 		{
 			echo "<tr>";
 			echo "<td>".$channel->name."</td>";
-			echo "<td>".$channel->creation_time."</td>";
 			echo "<td>".$channel->num_users."</td>";
-			$topic = (isset($channel->topic)) ? $channel->topic : "";
-			echo "<td>".$topic."</td>";
-			$setby = (isset($channel->topic)) ? "By ".$channel->topic_set_by .", at ".$channel->topic_set_at : "";
-			echo "<td>".$setby."</td>";
 			$modes = (isset($channel->modes)) ? "+" . $channel->modes : "<none>";
 			echo "<td>".$modes."</td>";
+			$topic = (isset($channel->topic)) ? $channel->topic : "";
+			echo "<td>".$topic."</td>";
+			echo "<td>".$channel->creation_time."</td>";
 		}
 	?>
 </tbody>
