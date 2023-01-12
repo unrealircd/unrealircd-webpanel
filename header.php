bca6dbd294c4e7738924e8ccfaec68129f76b1b3
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2023-01-12 21:17:48 +0000

    Include font-awesome

--- header.php
+++ header.php
@@ -12,6 +12,9 @@
 <!-- Latest compiled JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
+<!-- Font Awesome icons -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
 <script src="js/unrealircd-admin.js"></script>
 <title>UnrealIRCd Panel</title>
 <link rel="icon" type="image/x-icon" href="<?php echo BASE_URL; ?>img/favicon.ico">
