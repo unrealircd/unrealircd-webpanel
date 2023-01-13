ca8cae9d83f0667f179350b057f6c57f253bd782
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2023-01-13 03:44:21 +0000

    Play with alert messages

--- Classes/class-message.php
+++ Classes/class-message.php
@@ -5,25 +5,25 @@ class Message
 	static function Fail($message)
 	{
 		?>
-		<div class="alert alert-danger" role="alert">
-		<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+		<div class="alert alert-short alert-danger fade show" role="alert">
 		<?php echo $message; ?>
+		<span class="closebtn text-right" onclick="this.parentElement.style.display='none';">&times;</span>
 	  </div> <?php
 	}
 	static function Success($message)
 	{
 		?>
-		<div class="alert alert-success" role="alert">
-		<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+		<div class="alert alert-success fade show" role="alert">
 		<?php echo $message; ?>
+		<span class="closebtn text-right" onclick="this.parentElement.style.display='none';">&times;</span>
 	  </div> <?php
 	}
 	static function Info($message)
 	{
 		?>
-		<div class="alert alert-info" role="alert">
-		<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+		<div class="alert alert-info fade show" role="alert">
 		<?php echo $message; ?>
+		<span class="closebtn text-right" onclick="this.parentElement.style.display='none';">&times;</span>
 	  </div> <?php
 	}
 }
