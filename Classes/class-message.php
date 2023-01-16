f599de67ca1bee0516a2b720bfc4268d59e3223b
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2023-01-16 05:24:47 +0000

    Shorten Message notifications

--- Classes/class-message.php
+++ Classes/class-message.php
@@ -2,28 +2,49 @@
 
 class Message
 {
-	static function Fail($message)
+	static function Fail(...$message)
 	{
 		?>
 		<div class="alert alert-short alert-danger fade show" role="alert">
-		<?php echo $message; ?>
 		<span class="closebtn text-right" onclick="this.parentElement.style.display='none';">&times;</span>
+		<?php
+			for ($i = 0; isset($message[$i]); $i++)
+			{
+				echo $message[$i];
+				if (isset($message[$i + 1]))
+					echo "<br>";
+			}
+		?>
 	  </div> <?php
 	}
-	static function Success($message)
+	static function Success(...$message)
 	{
 		?>
-		<div class="alert alert-success fade show" role="alert">
-		<?php echo $message; ?>
+		<div class="alert alert-short alert-success fade show" role="alert">
 		<span class="closebtn text-right" onclick="this.parentElement.style.display='none';">&times;</span>
+		<?php
+			for ($i = 0; isset($message[$i]); $i++)
+			{
+				echo $message[$i];
+				if (isset($message[$i + 1]))
+					echo "<br>";
+			}
+		?>
 	  </div> <?php
 	}
-	static function Info($message)
+	static function Info(...$message)
 	{
 		?>
-		<div class="alert alert-info fade show" role="alert">
-		<?php echo $message; ?>
+		<div class="alert alert-short alert-info fade show" role="alert">
 		<span class="closebtn text-right" onclick="this.parentElement.style.display='none';">&times;</span>
+		<?php
+			for ($i = 0; isset($message[$i]); $i++)
+			{
+				echo $message[$i];
+				if (isset($message[$i + 1]))
+					echo "<br>";
+			}
+		?>
 	  </div> <?php
 	}
 }
