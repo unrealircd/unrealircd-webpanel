cbcb570ba9d0aee84d969f2ab8d3601c564d967b
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2023-01-16 06:43:24 +0000

    Add Message::Warning

--- Classes/class-message.php
+++ Classes/class-message.php
@@ -47,4 +47,19 @@ class Message
 		?>
 	  </div> <?php
 	}
+	static function Warning(...$message)
+	{
+		?>
+		<div class="alert alert-short alert-warning fade show" role="alert">
+		<span class="closebtn text-right" onclick="this.parentElement.style.display='none';">&times;</span>
+		<?php
+			for ($i = 0; isset($message[$i]); $i++)
+			{
+				echo $message[$i];
+				if (isset($message[$i + 1]))
+					echo "<br>";
+			}
+		?>
+	  </div> <?php
+	}
 }
