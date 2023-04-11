57190f5908d879e90cd5b3296f5fc73fe8ad7e1a
Valerie Pond <v.a.pond@outlook.com>
2023-04-11 01:12:40 +0100

    beginnings of api/notification

---
+++ api/notification.php
@@ -0,0 +1,46 @@
+<?php
+include "../common.php";
+include "../connection.php";
+
+
+session_start();
+if (!isset($_SESSION['id']))
+    die("Access denied");
+
+// Close the session now, otherwise other pages block
+session_write_close();
+
+// Set a valid header so browsers pick it up correctly.
+//header('Content-type: text/html; charset=utf-8');
+header("Content-type: application/json");
+
+// Explicitly disable caching so Varnish and other upstreams won't cache.
+header("Cache-Control: no-cache, must-revalidate");
+
+// Setting this header instructs Nginx to disable fastcgi_buffering and disable
+// gzip for this request.
+header('X-Accel-Buffering: no');
+
+// No time limit
+set_time_limit(0);
+
+// Send content immediately
+ob_implicit_flush(1);
+
+// Eh.. yeah...
+ob_end_flush();
+
+// If we use fastcgi, then finish the request now (UNTESTED)
+if (function_exists('fastcgi_finish_request'))
+    fastcgi_finish_request();
+
+$sources = ["!debug","all"];
+$rpc->log()->subscribe($sources);
+echo $rpc->error;
+for(;;)
+{
+    $res = $rpc->eventloop();
+    if (!$res)
+        continue;
+    echo json_encode($res)."\n";
+}
