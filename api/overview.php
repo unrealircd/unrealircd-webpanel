c9eb82da525f0e72686a4fc9ae208ae3728690e0
Bram Matthys <syzop@vulnscan.org>
2023-04-12 08:17:22 +0200

    Close session early in the api/overview.php so it doesn't block other pages.
    This closes the session before connecting to the server and doing the stats
    call, which may take a few hundred milliseconds on long distance servers.
    
    Also writes a JSON header so i can see the result easily in my browser ;D

--- api/overview.php
+++ api/overview.php
@@ -5,7 +5,13 @@ if (!isset($_SESSION["id"]))
     die("{\"error\": \"Access denied\"}");
 
 include "../common.php";
+
+// Close the session now, otherwise other pages block too long
+session_write_close();
+
 include "../connection.php";
+
+header("Content-type: application/json");
 
 $stats = $rpc->query("stats.get", []);
 echo json_encode($stats);
