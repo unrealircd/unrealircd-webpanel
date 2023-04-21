7cc3b4a0dee9eb5b44d33d9d0a4fa5e8b93eb1ef
Bram Matthys <syzop@vulnscan.org>
2023-04-21 11:55:06 +0200

    Slight miscommunication :D

--- plugins/sql_auth/SQL/settings.php
+++ plugins/sql_auth/SQL/settings.php
@@ -3,7 +3,7 @@ class DbSettings {
 	public static function get()
 	{
 		$conn = sqlnew();
-		$query = "SELECT * FROM " . get_config("mysql::table_prefix") . "auth_settings";
+		$query = "SELECT * FROM " . get_config("mysql::table_prefix") . "settings";
 		$result = $conn->query($query);
 		$list = [];
 		while ($row = $result->fetch())
@@ -15,17 +15,17 @@ class DbSettings {
 	public static function set($key, $val) : int
 	{
 		$conn = sqlnew();
-		$stmt = $conn->prepare("SELECT * FROM " . get_config("mysql::table_prefix") . "auth_settings WHERE setting_key = :name LIMIT 1");
+		$stmt = $conn->prepare("SELECT * FROM " . get_config("mysql::table_prefix") . "settings WHERE setting_key = :name LIMIT 1");
 		$stmt->execute(["name" => $key]);
 		if ($stmt->rowCount()) // if it already exists update it
-			$stmt = $conn->prepare("UPDATE " . get_config("mysql::table_prefix") . "auth_settings SET setting_value = :value WHERE setting_key = :name");
+			$stmt = $conn->prepare("UPDATE " . get_config("mysql::table_prefix") . "settings SET setting_value = :value WHERE setting_key = :name");
 			
 		else // otherwise create it
-			$stmt = $conn->prepare("INSERT INTO " . get_config("mysql::table_prefix") . "auth_settings (setting_key, setting_value) VALUES (:name, :value)");
+			$stmt = $conn->prepare("INSERT INTO " . get_config("mysql::table_prefix") . "settings (setting_key, setting_value) VALUES (:name, :value)");
 
 		// make sure it's there/correct
 		$stmt->execute(["name" => $key, "value" => serialize($val)]);
-		$stmt = $conn->prepare("SELECT * FROM " . get_config("mysql::table_prefix") . "auth_settings WHERE setting_key = :name LIMIT 1");
+		$stmt = $conn->prepare("SELECT * FROM " . get_config("mysql::table_prefix") . "settings WHERE setting_key = :name LIMIT 1");
 		$stmt->execute(["name" => $key]);
 		return $stmt->rowCount(); // return 1 or 0 bool-like int
 	}
