582e5c7479087f37c2002d3e3d1dfe73e6db0381
Bram Matthys <syzop@vulnscan.org>
2023-04-25 10:32:59 +0200

    Add js/datatables-ellipsis.js for automatic truncating of table fields (eg: reason)

--- inc/header.php
+++ inc/header.php
@@ -38,6 +38,7 @@ $arr = []; Hook::run(HOOKTYPE_PRE_HEADER, $arr);
 <script src="<?php echo get_config("base_url"); ?>js/unrealircd-admin.js"></script>
 <script src="<?php echo get_config("base_url"); ?>js/datatables.min.js"></script>
 <script src="<?php echo get_config("base_url"); ?>js/datatables-natural-sort.js"></script>
+<script src="<?php echo get_config("base_url"); ?>js/datatables-ellipsis.js"></script>
 <script>
 		var BASE_URL = "<?php echo get_config("base_url"); ?>";
 		function timeoutCheck() {
