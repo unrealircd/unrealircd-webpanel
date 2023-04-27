7bfda0e1316d5024d3871cc6e38a6920106d5907
Bram Matthys <syzop@vulnscan.org>
2023-04-27 20:37:52 +0200

    Fetch existing logs in Log screen. Only for git unrealircd. Or actually this isn't even committed yet.... Requires end users to run 'composer install' as well...

--- api/log.php
+++ api/log.php
@@ -6,4 +6,13 @@ if (!$rpc)
     die();
 
 /* Basically everything ;) */
+
+$response = $rpc->log()->getAll();
+if ($response !== false)
+{
+    /* Only supported in later UnrealIRCd versions */
+    foreach($response as $r)
+        send_sse($r);
+}
+
 api_log_loop(["all", "!debug"]);
