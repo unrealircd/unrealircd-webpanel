27fba7fa81b6ffc56ad028271ba2b077ace73b1a
Bram Matthys <syzop@vulnscan.org>
2023-05-05 15:00:18 +0200

    Logs: don't show join/part/kick in live view by default.
    See comment...
    /* Add these as well, they are not logged by default
     * in the memory log either. See
     * https://github.com/unrealircd/unrealircd/commit/45342c2d33968178cd07a12cd6fdc4e65b604134
     * Added here separately because we may want to make
     * this an option...
     */

--- api/log.php
+++ api/log.php
@@ -5,7 +5,22 @@ require_once('common_api.php');
 if (!$rpc)
     die();
 
-/* Basically everything ;) */
+/* Filter - almost everything... */
+$log_list = ["all", "!debug"];
+/* Add these as well, they are not logged by default
+ * in the memory log either. See
+ * https://github.com/unrealircd/unrealircd/commit/45342c2d33968178cd07a12cd6fdc4e65b604134
+ * Added here separately because we may want to make
+ * this an option...
+ */
+array_push($log_list,
+    "!join.LOCAL_CLIENT_JOIN",
+    "!join.REMOTE_CLIENT_JOIN",
+    "!part.LOCAL_CLIENT_PART",
+    "!part.REMOTE_CLIENT_PART",
+    "!kick.LOCAL_CLIENT_KICK",
+    "!kick.REMOTE_CLIENT_KICK",
+);
 
 $response = $rpc->log()->getAll();
 if ($response !== false)
@@ -25,4 +40,4 @@ if ($response !== false)
 $r = ["sync_option"=>"sync_now"];
 send_sse($r);
 
-api_log_loop(["all", "!debug"]);
+api_log_loop($log_list);
