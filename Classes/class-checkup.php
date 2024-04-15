7f7c0b81040eaea3686c2c27d0b540a80c8a25b5
Valerie Pond <v.a.pond@outlook.com>
2024-04-15 03:40:53 +0100

    Network Health
    Adds a network health widget to the overview and a page for listing all the shit you did wrong

--- Classes/class-checkup.php
+++ Classes/class-checkup.php
@@ -1,132 +1,293 @@
 <?php
-
+require_once "../inc/connection.php";
 
 /**
  * Does a complete checkup of the network.
  */
 class CheckUp
 {
-    public $num_of_problems = [
-        "chanmodes" => 0,
-        "usermodes" => 0,
-        "modules" => 0,
-        "other" => 0,
-    ];
-    public $problems = [
-        "chanmodes" => [],
-        "usermodes" => [],
-        "modules" => [],
-        "other" => [],
-    ];
+	const SCORE_PERFECT = 0;
+	const SCORE_NOT_BAD = 1;
+	const SCORE_COULD_BE_BETTER = 10;
+	const SCORE_NEEDS_ATTENTION = 25;
+	const SCORE_VERY_BAD = 50;
 
-    public $serverlist = [];
+	public $num_of_problems = [
+		"chanmodes" => 0,
+		"usermodes" => 0,
+		"modules" => 0,
+		"servers" => 0,
+		"other" => 0,
+	];
+	public $problems = [
+		"chanmodes" => [],
+		"usermodes" => [],
+		"modules" => [],
+		"servers" => [],
+		"other" => [],
+	];
 
-    /**
-     * Construct0r
-     */
-    function __construct()
-    {
-        global $rpc;
+	public $serverlist = [];
 
-        $this->serverlist = $rpc->server()->getAll();
-        $this->chanmode_check();
-        $this->usermode_check();
-        //$this->module_check();
-        
-    }
+	/**
+	 * Construct0r
+	 */
+	function __construct()
+	{
+		global $rpc;
 
-    /**
-     * Checks channel modes of servers against other servers
-     * @return void
-     */
-    function chanmode_check() : void
-    {
-        foreach($this->serverlist as $s) // cycle through each server
-        {
-            /* make a single string from the array of groups */
-            $ourchmodes = "";
-            foreach ($s->server->features->chanmodes as $set)
-                for ($i=0; isset($set[$i]); $i++)
-                    strcat($ourchmodes,$set[$i]);
+		$this->serverlist = $rpc->server()->getAll();
+		$this->chanmode_check();
+		$this->usermode_check();
+		$this->module_check();
+		$this->server_check();
+			
+	}
 
-            /* take a look at every other server... yep, we do this for every server */
-            foreach ($this->serverlist as $serv)
-            {
-                /* except for ourselves lol */
-                if ($serv->id == $s->id)
-                    continue;
-                
-                /* hmm if it's not unreal, skip it too */
-                if (!strstr($serv->server->features->software,"UnrealIRCd"))
-                    continue;
-                
-                /* make a single string from the array of groups but for them this time */
-                $theirchmodes = "";
-                foreach ($serv->server->features->chanmodes as $set)
-                    for ($i=0; isset($set[$i]); $i++)
-                        strcat($theirchmodes,$set[$i]);
+	public function total()
+	{
+		$count = 0;
+		foreach ($this->num_of_problems as $problem)
+			$count += $problem;
+		return $count;
+	}
 
-                /* check ours against theirs */
-                for ($i=0; isset($ourchmodes[$i]) && $m = $ourchmodes[$i]; $i++)
-                {
-                    /* if we have a mode that they don't have */
-                    if (!strstr($theirchmodes, $m))
-                    {
-                        ++$this->num_of_problems['chanmodes'];
-                        $this->problems['chanmodes'][] = "Channel mode $m is present on $s->name but missing on $serv->name";
-                    }
-                }
-            }
-        }
-    }
+	public function equivalate()
+	{
+		$total = $this->total();
+		if ($total == self::SCORE_PERFECT)
+				return "Perfect";
+		elseif ($total < self::SCORE_COULD_BE_BETTER)
+				return "Not Bad";
+		elseif ($total < self::SCORE_NEEDS_ATTENTION)
+				return "Could Be Better";
+		elseif ($total < self::SCORE_VERY_BAD)
+				return "Needs Attention";
+		else return "Very Poor";
+	}
+	public function badgestyle()
+	{
+		$total = $this->total();
+		if ($total == self::SCORE_PERFECT)
+				return "success";
+		elseif ($total < self::SCORE_COULD_BE_BETTER)
+				return "info";
+		elseif ($total < self::SCORE_NEEDS_ATTENTION)
+				return "warning";
+		elseif ($total < self::SCORE_VERY_BAD)
+				return "danger";
+		else return "dark";
 
-    /**
-     * Checks user modes of servers against other servers
-     * @return void
-     */
-    function usermode_check() : void
-    {
-         /* make a single string from the array of groups */
-         $ourumodes = $s->server->features->usermodes;
+	}
 
-        /* take a look at every other server... yep, we do this for every server */
-        foreach ($this->serverlist as $serv)
-        {
-            /* except for ourselves lol */
-            if ($serv->id == $s->id)
-                continue;
-            
-            /* hmm if it's not unreal, skip it too */
-            if (!strstr($serv->server->features->software,"UnrealIRCd"))
-                continue;
-            
-            $theirumodes = $serv->server->features->usermodes;
+	/**
+	 * Checks channel modes of servers against other servers
+	 * @return void
+	 */
+	function chanmode_check() : void
+	{
+		foreach($this->serverlist as $s) // cycle through each server
+		{
+			/* hmm if it's not unreal, skip it too */
+			if (!strstr($s->server->features->software,"UnrealIRCd"))
+				continue;
+			/* make a single string from the array of groups */
+			$ourchmodes = "";
+			foreach ($s->server->features->chanmodes as $set)
+				for ($i=0; isset($set[$i]); $i++)
+					strcat($ourchmodes,$set[$i]);
 
-            /* check ours against theirs */
-            for ($i=0; isset($ourumodes[$i]) && $m = $ourumodes[$i]; $i++)
-            {
-                /* if we have a mode that they don't have */
-                if (!strstr($theirumodes, $m))
-                {
-                    ++$this->num_of_problems['usermodes'];
-                    $this->problems['usermodes'][] = "User mode $m is present on $s->name but missing on $serv->name";
-                }
-            }
-        }
-    }
+			/* take a look at every other server... yep, we do this for every server */
+			foreach ($this->serverlist as $serv)
+			{
+				/* except for ourselves lol */
+				if ($serv->id == $s->id)
+					continue;
+				
+				/* hmm if it's not unreal, skip it too */
+				if (!strstr($serv->server->features->software,"UnrealIRCd"))
+					continue;
+				
+				/* make a single string from the array of groups but for them this time */
+				$theirchmodes = "";
+				foreach ($serv->server->features->chanmodes as $set)
+					for ($i=0; isset($set[$i]); $i++)
+						strcat($theirchmodes,$set[$i]);
 
-    /**
-     * Checks modules of servers against other servers
-     * @return void
-     */
-    function module_check() : void
-    {
-        global $rpc;
-        $modlist = [];
-        $modarray = [];
-        foreach ($this->serverlist as $serv)
-            $modlist[$serv->name] = json_decode(json_encode($rpc->server()->module_list($serv->id)->list), true);
+				/* check ours against theirs */
+				for ($i=0; isset($ourchmodes[$i]) && $m = $ourchmodes[$i]; $i++)
+				{
+					/* if we have a mode that they don't have */
+					if (!strstr($theirchmodes, $m))
+					{
+						++$this->num_of_problems['chanmodes'];
+						$this->problems['chanmodes'][] = "Channel mode <code>$m</code> is present on <code>$s->name</code> but missing on <code>$serv->name</code>";
+					}
+				}
+			}
+		}
+	}
 
-        echo highlight_string(var_export($modlist,true));
-    }
+	/**
+	 * Checks user modes of servers against other servers
+	 * @return void
+	 */
+	function usermode_check() : void
+	{
+		foreach($this->serverlist as $s)
+		{
+			/* hmm if it's not unreal, skip it too */
+			if (!strstr($s->server->features->software,"UnrealIRCd"))
+				continue;
+			/* make a single string from the array of groups */
+			$ourumodes = $s->server->features->usermodes;
+
+			/* take a look at every other server... yep, we do this for every server */
+			foreach ($this->serverlist as $serv)
+			{
+				/* except for ourselves lol */
+				if ($serv->id == $s->id)
+					continue;
+				
+				/* hmm if it's not unreal, skip it too */
+				if (!strstr($serv->server->features->software,"UnrealIRCd"))
+					continue;
+				
+				$theirumodes = $serv->server->features->usermodes;
+
+				/* check ours against theirs */
+				for ($i=0; isset($ourumodes[$i]) && $m = $ourumodes[$i]; $i++)
+				{
+					/* if we have a mode that they don't have */
+					if (!strstr($theirumodes, $m))
+					{
+						++$this->num_of_problems['usermodes'];
+						$this->problems['usermodes'][] = "User mode <code>$m</code> is present on <code>$s->name</code> but missing on <code>$serv->name</code>";
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Checks modules of servers against other servers
+	 * @return void
+	 */
+	function module_check() : void
+	{
+		global $rpc;
+		foreach ($this->serverlist as $s)
+		{
+			/* hmm if it's not unreal, skip it too */
+			if (!strstr($s->server->features->software,"UnrealIRCd"))
+				continue;
+			$ourmods = sort_mods(json_decode(json_encode(@$rpc->server()->module_list($s->id)->list), true));
+			
+			// doesn't support that yet
+			if (empty($ourmods))
+				continue;
+
+			foreach ($this->serverlist as $serv)
+			{
+				/* except for ourselves lol */
+				if ($serv->id == $s->id)
+					continue;
+				
+				/* hmm if it's not unreal, skip it too */
+				if (!strstr($serv->server->features->software,"UnrealIRCd"))
+					continue;
+
+				$theirmods = sort_mods(json_decode(json_encode(@$rpc->server()->module_list($serv->id)->list), true));
+
+				// doesn't support that yet
+				if (empty($theirmods))
+					continue;
+				// we only check if theirs doesn't match ours
+				foreach ($theirmods as $name => $version)
+				{
+					if (!isset($ourmods[$name])) // we don't have that module
+					{
+						++$this->num_of_problems['modules'];
+						$this->problems['modules'][] = "Module <code>$name</code> exists on <code>$serv->name</code> but not <code>$s->name</code>";
+					}
+					else if ((int)$version > (int)$ourmods[$name])
+					{
+						++$this->num_of_problems['modules'];
+						$this->problems['modules'][] = "Module <code>$name</code> on <code>$serv->name</code> is newer than on <code>$s->name</code>";
+					}
+				}
+			}
+		}
+	}
+
+	function server_check() : void
+	{
+		global $rpc;
+		foreach ($this->serverlist as $s)
+		{
+			/* hmm if it's not unreal, skip it too */
+			if (!strstr($s->server->features->software,"UnrealIRCd"))
+					continue;
+			// protocol checking
+			$ours = (int)$s->server->features->protocol;
+			foreach ($this->serverlist as $serv)
+			{
+				if (!strstr($serv->server->features->software,"UnrealIRCd"))
+					continue;
+
+				$theirs = (int)$serv->server->features->protocol;
+
+				if ($ours < $theirs)
+				{
+					++$this->num_of_problems['servers'];
+					$this->problems['servers'][] = "Protocol mismatch: <code>$serv->name</code> using protocol <code>$theirs</code> but <code>$s->name</code> using protocol <code>$ours</code>. <a href=\"https://www.unrealircd.org/docs/Upgrading\">Click for upgrade documentation.</a>";
+				}
+			}
+
+			// EOL checking
+			$tok = explode('-', $s->server->features->software);
+			if ((int)$tok[1] < 6)
+			{
+				++$this->num_of_problems['servers'];
+				$this->problems['servers'][] = "EOL: <code>$s->name</code> (".$s->server->features->software.") is running old unsupported software which is no longer receiving security updates. <a href=\"https://www.unrealircd.org/docs/Upgrading\">Click here for upgrade documentation.</a>";
+			}
+		}
+	}
+	/* Print a widget easy! */
+	function __toString()
+	{
+		return '
+		<a style="width:fit-content" id="health_banner" class="card alert text-dark alert-'.(!$this->total() ? "success" : "danger").'" role="alert" href="'.get_config("base_url").'tools/checkup.php">
+			<i class="fa-solid fa-heart-pulse fa-2x" style="position:absolute;top:10px;left:10px"></i>
+			<h4 class="alert-heading mt-0 mb-0" style="padding-left:40px;">Network Health </h4><span style="position:relative;top:5px;" class="ml-3 badge badge-'.$this->badgestyle().'">'.$this->equivalate().'</span>
+			<p class="ml-4 mt-2 mb-0">Found <b>'.$this->total().'</b> problems in total.</p>
+	</a>';
+	}
+
+	public function toTable(Array $array)
+	{
+		echo "<table class=\"table table-striped\">\n";
+		foreach ($array as $key => $value)
+		{
+			echo "\t<tr><td>$value</td></tr>\n";
+		}
+		echo "</table>\n";
+	}
+}
+
+function sort_mods($mods)
+{
+		$list = [];
+		if (!$mods)
+				return $list;
+		foreach($mods as $mod)
+				$list[$mod["name"]] = $mod["version"];
+		
+		return $list;
+}
+
+function checkup_widget()
+{
+		$ch = new CheckUp();
+		echo $ch;
 }
--- tools/checkup.php
+++ tools/checkup.php
@@ -5,160 +5,80 @@ require_once "../inc/header.php";
 require_once "../Classes/class-checkup.php";
 
 $checkup = new CheckUp();
-
 ?>
 
 <h4>Network Health Checkup</h4>
+<style>
+	.card {
+		min-height: 80%;
+		border-radius: 16px;
+	}
+	#accordion > .card {
+		margin-bottom: 5px;
+		min-width: 400px;
+		width:fit-content;
+	}
 
-<div class="container">
-
-<div class="row mt-3">
-    <div class="col-sm mb-3">
-        <div class="card text-center">
-            <div class="card-header bg-<?php echo ($checkup->num_of_problems['chanmodes']) ? "danger" : "success"; ?> text-white">
-                <div class="row">
-                    <div class="col">
-                        <i class="fa fa-hashtag fa-3x"></i>
-                    </div>
-                    <div class="col">
-                        <h3 class="display-4"><?php echo $checkup->num_of_problems['chanmodes']; ?></h3><div class="display-5">problems</div>
-                    </div>
-                </div>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col">
-                        <h6>Channel Modes</h6>
-                    </div>
-                    <div class="col"> <a class="btn btn-primary">View</a></div>
-                </div>
-            </div>
-        </div>
-        
-
-    </div>
-    <div class="col-sm mb-3">
-        <div class="card text-center">
-            <div class="card-header bg-<?php echo ($checkup->num_of_problems['usermodes']) ? "danger" : "success"; ?> text-white">
-                <div class="row">
-                    <div class="col">
-                        <i class="fa fa-user fa-3x"></i>
-                    </div>
-                    <div class="col">
-                        <h3 class="display-4"><?php echo $checkup->num_of_problems['usermodes']; ?></h3><div class="display-5">problems</div>
-                    </div>
-                </div>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col">
-                        <h6>User Modes</h6>
-                    </div>
-                    <div class="col"><a class="btn btn-primary">View</a></div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm mb-3">
-        <div class="card text-center">
-            <div class="card-header bg-warning">
-                <div class="row">
-                    <div class="col">
-                        <i class="fa fa-plug fa-3x"></i>
-                    </div>
-                    <div class="col">
-                        <h3 class="display-4"><?php // nothing ?></h3>
-                    </div>
-                </div>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col">
-                        <h6>Modules</h6>
-                    </div>
-                    <div class="col"><a class="btn btn-primary">View</a></div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-sm mb-3">
-        <div class="card text-center">
-            <div class="card-header bg-secondary text-white">
-                <div class="row">
-                    <div class="col">
-                        <i class="fa fa-network-wired fa-3x"></i>
-                    </div>
-                    <div class="col">
-                        <h3 class="display-4"><?php // nothing ?></h3>
-                    </div>
-                </div>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col">
-                        <h6>Servers</h6>
-                    </div>
-                    <div class="col"> <a class="btn btn-primary">View</a></div>
-                </div>
-            </div>
-        </div>
-    </div>
+</style>
+<?php echo $checkup ?>
+<div id="accordion">
+	<div class="card">
+		<div class="card-header" id="headingOne" aria-describedby="test1">
+			<h5 class="mb-0">
+				<button class="btn" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+					User Mode Conflicts <span class="badge badge-danger"><?php echo $checkup->num_of_problems['usermodes'] ?></span>
+				</button>
+			</h5>
+		</div>
+		<div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+			<div class="card-body">
+				<?php $checkup->toTable($checkup->problems['usermodes']); ?>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-header" id="headingTwo">
+			<h5 class="mb-0">
+				<button class="btn collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+					Channel Mode Conflicts <span class="badge badge-danger"><?php echo $checkup->num_of_problems['chanmodes'] ?></span>
+				</button>
+			</h5>
+		</div>
+		<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+			<div class="card-body">
+				<?php $checkup->toTable($checkup->problems['chanmodes']); ?>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-header" id="headingThree">
+			<h5 class="mb-0">
+				<button class="btn collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+					Module Conflicts <span class="badge badge-danger"><?php echo $checkup->num_of_problems['modules'] ?></span>
+				</button>
+			</h5>
+		</div>
+		<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+			<div class="card-body">
+				<?php $checkup->toTable($checkup->problems['modules']); ?>
+			</div>
+		</div>
+	</div>
+	<div class="card">
+		<div class="card-header" id="headingFour">
+			<h5 class="mb-0">
+				<button class="btn collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+					Server Conflicts <span class="badge badge-danger"><?php echo $checkup->num_of_problems['servers'] ?></span>
+				</button>
+			</h5>
+		</div>
+		<div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+			<div class="card-body">
+				<?php $checkup->toTable($checkup->problems['servers']); ?>
+			</div>
+		</div>
+	</div>
 </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
-<body>
-<canvas id="myChart" style="width:100%;max-width:400px"></canvas>
+<?php
 
-<script id="js">
-
-var xValues = ["15 minutes", "5 minutes", "1 minute"];
-var yValues = [<?php echo "\"".$cpuUsage[2]."\", \"".$cpuUsage[1]."\", \"".$cpuUsage[0]."\""; ?>];
-var barColors = ["blue", "blue", "blue"]
-
-new Chart("myChart", {
-  type: "bar",
-  type: "bar",
-  data: {
-    labels: xValues,
-    datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }]
-  },
-
-  options: {
-    legend: {display: false},
-    title: {
-      display: true,
-      text: "CPU Usage",
-      fontSize: 16
-    },
-    scales: {
-      xAxes: [{ticks: {min: 0, max:15}}],
-      yAxes: [{ticks: {min: 0, max:1}}],
-    }
-  }
-});
-</script>
-<div id="stats-container">
-    <p id="cpu-usage">CPU Usage: </p>
-    <p id="memory-usage">Memory Usage: </p>
-</div>
-
-<script>
-    function updateStats() {
-        var xhttp = new XMLHttpRequest();
-        var BASE_URL = "<?php echo get_config("base_url"); ?>";
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                var data = JSON.parse(this.responseText);
-                document.getElementById("cpu-usage").innerHTML = "Current Usage: <code>" + data.cpu + "</code>";
-                document.getElementById("memory-usage").innerHTML = "Memory Usage: <code>" + data.memory + "</code>";
-            }
-        };
-        xhttp.open("GET", BASE_URL + "api/data.php", true);
-        xhttp.send();
-    }
-    updateStats();
-    setInterval(updateStats, 1000); // Update stats every second
-</script>
+require_once "../inc/footer.php";
