20d2aa3421da71c96892c926d3fafe39c0b66f16
Valerie Pond <v.a.pond@outlook.com>
2024-07-08 12:28:38 +0800

    Add a link to patreon on the footer

--- inc/footer.php
+++ inc/footer.php
@@ -11,13 +11,19 @@
 			echo "<small style=\"font-size: 70%\"><code>Admin Panel ".get_config('webpanel_version')."</code></small>";
 		?>
 
-		<section class="mt-1">
+		<section class="mt-1"><a href="https://patreon.com/UnrealIRCd/"
+				role="button"
+				data-mdb-ripple-color="dark"
+				target="_blank">
+				<img src="<?php echo get_config('base_url')?>/img/patreon.png" style="margin-right: 4px;" width="16" height="16"></a>
+
 			<a href="https://www.youtube.com/@unrealircdtutorials/"
 				role="button"
 				data-mdb-ripple-color="dark"
 				target="_blank">
-				<img src="https://static-00.iconduck.com/assets.00/youtube-icon-2048x2048-wiwalbpx.png" width="16" height="16"></a>
-
+				<img src="https://static-00.iconduck.com/assets.00/youtube-icon-2048x2048-wiwalbpx.png"width="16" height="16"></a>
+				
+			
 			<!-- Twitter -->
 			<a
 				class="btn btn-link btn-floating btn-md text-white"
@@ -25,7 +31,7 @@
 				role="button"
 				data-mdb-ripple-color="dark"
 				target="_blank"
-				><i class="fab fa-twitter"></i
+				><i class="fab fa-twitter" style="margin-left: -7px; margin-right:-4px" ></i
 			></a>
 			<!-- Github -->
 			<a
@@ -42,7 +48,7 @@
 				role="button"
 				data-mdb-ripple-color="dark"
 				target="_blank">
-				<img src="<?php echo get_config("base_url"); ?>img/unreal.jpg" width="16" height="16" style="margin-left: -7px; margin-right: 17px"></a>
+				<img src="<?php echo get_config("base_url"); ?>img/unreal.jpg" width="16" height="16" style="margin-left: -7px;"></a>
 
 		<?php
 
