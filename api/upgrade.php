2f40a4cc30bf03b1e93c76fffe19a147bd3f7b15
Valerie Pond <v.a.pond@outlook.com>
2024-08-10 13:39:53 +0800

    Upgrading: Return on any failure

--- api/upgrade.php
+++ api/upgrade.php
@@ -17,9 +17,10 @@ if (Upgrade::$upgrade_available)
 {
     error_log("Upgrade available, downloading and installing");
     if (!$upgrade->downloadUpgradeZip())
-        error_log($upgrade->error);
+        return error_log($upgrade->error);
+    
     else if (!$upgrade->extractZip())
-        error_log($upgrade->error);
+        return error_log($upgrade->error);
         
     $upgrade->cleanupOldFiles();
     
